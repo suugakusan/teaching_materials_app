c560d474fedf82e3683a5d6fc8a579076b189712
tomohiro.kojima <niniseseko@gmail.com>
2021-11-11 10:35:36 +0000

    user-showのテスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  
   describe "GET #new" do
     subject { get(signup_path) }
     it "リクエストが成功する" do
       subject
       expect(response).to have_http_status(:ok)
+    end
+  end
+  
+  describe "GET #show" do
+    subject { get(user_path(user_id)) }
+    context "ユーザーが存在するとき" do
+      before do
+         post login_path, params: { session: {
+        email: user.email,
+        password: user.password,
+         } }
+      end
+      
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+      
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+      
+      it "name が表示されている" do
+        subject
+        expect(response.body).to include user.name
+      end
     end
   end
 end
